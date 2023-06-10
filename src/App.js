e7801800aa8ef80c71dd37afd26a417af1802232
Shankar K <shankar747@hotmail.com>
2023-06-10 19:05:01 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState("");
+
+  const getDataFromServer = () => {
+    fetch("http://localhost:5000/api")
+      .then((res) => res.text())
+      .then((data) => setData(data));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{data || "Hello, no-one"}</h1>
+        <button onClick={getDataFromServer}>Get Data</button>
       </header>
     </div>
   );
